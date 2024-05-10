07dd2293ff4569f89a7e8fc0ce2f1e1cb59aaee9
Pablo Bejarano <pablobejarano2112@gmail.com>
2024-05-10 19:17:47 -0300

    first commit

---
+++ src/main/java/ar/edu/unju/fi/Tp3BejaranoPabloApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp3BejaranoPabloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp3BejaranoPabloApplication.class, args);
+	}
+
+}
